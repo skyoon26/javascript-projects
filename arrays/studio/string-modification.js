0869420a0c84293243329be05517cd588b569085
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-01-19 15:59:46 -0600

    second attempt of string modification studio

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,23 +3,20 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-let oneStr = str.replace("Lau", "");
-let twoStr = str.slice(0 ,3);
-
-let finalStr = oneStr + twoStr
-
-console.log(finalStr);
+let newStr = str.replace("Lau", "") + str.slice(0, 3);
+console.log(newStr);
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
-console.log(`The originial string is "${str}" and the modified string is "${finalStr}"`);
+console.log(`The original name was ${str} but we turned it into ${newStr}!`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-let question = input.question("How many letters would you like to relocate? ");
-candidateAnswer = "";
+let info = input.question("How many letters would you like to move? ");
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
-if (question > 10) {
-    console.log(`There is an error, default to move 3 letters.`)
+if (info > 10) {
+    console.log(`Whoa! ${info} is way too many! There are only ${str.length} letters in ${str}. Will default to move 3.`);
+} else if (info <=10) {
+    console.log(`Awesome! We will move ${info} letters!`);
 } else {
-    console.log(`We will relocate ${question}.`);
+    console.log(`${info} is not a valid number!`);
 }
