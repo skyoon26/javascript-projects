c1449f22c98a1c3881f7f49506d8e418c8cf28a4
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-01-20 10:27:09 -0600

    third attempt of string modification studio

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,20 +3,22 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-let newStr = str.replace("Lau", "") + str.slice(0, 3);
-console.log(newStr);
+let newStr = str.slice(3) + str.slice(0, 3); //or can do: str.replace("Lau", "") + str.slice(0, 3);
+// console.log(newStr); returns "nchCodeLau"
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
-console.log(`The original name was ${str} but we turned it into ${newStr}!`);
+console.log(`The original name is ${str}, but we switched it up to ${newStr}. Try it out!`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-let info = input.question("How many letters would you like to move? ");
+let userAns = input.question("Please enter the number of letters to relocate: ");
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
-if (info > 10) {
-    console.log(`Whoa! ${info} is way too many! There are only ${str.length} letters in ${str}. Will default to move 3.`);
-} else if (info <=10) {
-    console.log(`Awesome! We will move ${info} letters!`);
+if (userAns >= str.length) {
+    console.log(`${userAns} is too many, as there are only ${str.length} letters in ${str}. 
+    Will default to 3 letters. Here is your result: ${newStr}`);
+} else if (userAns < str.length) {
+    let userStr = str.slice(userAns) + str.slice(0, userAns);
+    console.log(`We will move ${userAns} letters! This is your result: ${userStr}`);
 } else {
-    console.log(`${info} is not a valid number!`);
-}
+    console.log(`${userAns} is not a valid number!`);
+} 
