7a17b080600eae41ab00dcaf71393c34e411577c
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-01-25 14:23:47 -0600

    Attempted the bonus mission of the functions exercises by adding an optional character parameter

--- functions/exercises.js
+++ functions/exercises.js
@@ -1,51 +1,51 @@
 //Rectangles
-function makeLine(size) {
+function makeLine(size, char = '#') {
     let line = '';
     for (let i = 0; i < size; i++) {
-        line += '#';
+        line += char;
     }
     return line;
 }
 
 console.log(makeLine(5));
 
-function makeSquare(size) {
+function makeSquare(size, char = '#') {
     let square = '';
     // for (let i = 0; i < size; i++) {
     //     square += ('\n' + makeLine(size));
     // }
     let width = size, height = size;
-    square = makeRectangle(width, height)
+    square = makeRectangle(width, height, char)
     return square;
 }
 
 console.log(makeSquare(5));
 
-function makeRectangle(width, height) {
+function makeRectangle(width, height, char = '#') {
     let rectangle = '';
     for (let i = 0; i < height; i++) {
-        rectangle += (makeLine(width) + '\n');
+        rectangle += (makeLine(width, char) + '\n');
     }
     return rectangle.slice(0, -1);
 }
 
-console.log(makeRectangle(5,3));
+console.log(makeRectangle(5, 3));
 
-//Triangles
-function makeDownwardStairs(height) {
+// Triangles
+function makeDownwardStairs(height, char = '#') {
     let stairs = '';
     for (let i = 0; i < height; i++) {
-        stairs += (makeLine(i+1) + '\n');
+        stairs += (makeLine(i+1, char) + '\n');
     }
     return stairs.slice(0, -1);
 }
 
-console.log(makeDownwardStairs(5));
+// console.log(makeDownwardStairs(5, '!'));
 
-function makeSpaceLine(numSpaces, numChars) {
+function makeSpaceLine(numSpaces, numChars, char = '#') {
     let spaceLine = '', space = '', line = '';
     for (let i = 0; i < numChars; i++) {
-        line += "#";
+        line += char;
     }
     for (let j = 0; j < numSpaces; j++) {
         space += ' ';
@@ -54,26 +54,27 @@ function makeSpaceLine(numSpaces, numChars) {
     return spaceLine;
 }
 
-console.log(makeSpaceLine(3, 5));
+console.log(makeSpaceLine(3, 5, '*'));
 
-function makeIsoscelesTriangle(height) {
+function makeIsoscelesTriangle(height, char = '#') {
     let triangle = '';
     for (let i = 0; i < height; i++) {
-        triangle += (makeSpaceLine((height - i - 1), (2 * i + 1)) + '\n');
+        triangle += (makeSpaceLine((height - i - 1), (2 * i + 1), char) + '\n');
     }
     return triangle.slice(0, -1);
 }
 
-console.log(makeIsoscelesTriangle(5));
+console.log(makeIsoscelesTriangle(5, '^'));
 
 //Diamonds
 
-function makeDiamond(height) {
+function makeDiamond(height, char = '#') {
     let diamond = '';
     for (let i = 0; i < height; i++) {
-        diamond += makeSpaceLine((height - i - 1), (2 * i + 1)) + '\n';
+        diamond += makeSpaceLine((height - i - 1), (2 * i + 1), char) + '\n';
     }
     return diamond.slice(0, -1) + diamond.split('').reverse().join('');
 }
 
-console.log(makeDiamond(5));
+
+console.log(makeDiamond(5, '+'));
