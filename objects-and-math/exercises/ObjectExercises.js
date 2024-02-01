c6ef088cc284e8ccaa79fdaac02be16363c45e25
Deanne Chae <126113364+skyoon26@users.noreply.github.com>
2024-02-01 13:04:58 -0600

    Completed exercises for chapter 12

---
+++ objects-and-math/exercises/ObjectExercises.js
@@ -0,0 +1,73 @@
+let superChimpOne = {
+   name: "Chad",
+   species: "Chimpanzee",
+   mass: 9,
+   age: 6,
+   astronautID: 1,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let salamander = {
+   name: "Lacey",
+   species: "Axolotl Salamander",
+   mass: 0.1,
+   age: 5,
+   astronautID: 2,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let chimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let microBear = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+// After you have created the other object literals, add the astronautID property to each one.
+
+// Create an array to hold the animal objects.
+let crew = [superChimpOne, salamander, chimpTwo, dog, microBear];
+
+// Print out the relevant information about each animal.
+function crewReports(animal) {
+   return console.log(`${animal.name} is a ${animal.species}. They are ${animal.age} years old and ${animal.mass} kilograms. Their ID is ${animal.astronautID}.`);
+}
+
+crewReports(superChimpOne);
+
+// Start an animal race!
+function fitnessTest(arr) {
+   let results = [], numSteps, turns;
+   for (let i = 0; i < arr.length; i++) {
+      numSteps = 0;
+      turns = 0;
+      while (numSteps < 20) {
+         numSteps += arr[i].move();
+         turns++;
+      }
+      results.push(`${arr[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+}
+
+console.log(fitnessTest(crew));
